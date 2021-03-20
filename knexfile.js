551577b0e5f99e15884fc4220f77eb6e0f40da12
sergiomscp <46605629+sergiomscp@users.noreply.github.com>
2021-03-19 21:39:20 -0300

    Migrates com Knex V.1.0

---
+++ knexfile.js
@@ -0,0 +1,18 @@
+module.exports = {
+    client: 'postgresql',
+    connection: {
+        database: 'knowledge',
+        user: 'postgres',
+        password: 'root'
+
+    },
+    pool: {
+        min: 2,
+        max: 10
+    },
+
+    migrations: {
+        tableName: 'knex_migrations'
+    }
+
+};
